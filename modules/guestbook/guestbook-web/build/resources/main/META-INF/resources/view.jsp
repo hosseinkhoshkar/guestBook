64114e470fb775a0dff9b339994dd2e9b19bcc4a
hossein <h.khoshkar@gmail.com>
2021-06-02 11:45:57 +0430

    init commit

---
+++ modules/guestbook/guestbook-web/build/resources/main/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/META-INF/resources/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="guestbookweb.caption"/></b>
+</p>
---
+++ modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookWebPortlet.java
@@ -0,0 +1,40 @@
+package com.liferay.docs.guestbook.portlet.portlet;
+
+import com.liferay.docs.guestbook.portlet.constants.GuestbookWebPortletKeys;
+
+import com.liferay.docs.guestbook.service.GuestbookEntryLocalService;
+import com.liferay.docs.guestbook.service.GuestbookLocalService;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author hossein
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=GuestbookWeb",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + GuestbookWebPortletKeys.GUESTBOOKWEB,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+
+	},
+	service = Portlet.class
+)
+public class GuestbookWebPortlet extends MVCPortlet {
+
+	@Reference
+	private GuestbookEntryLocalService _guestbookEntryLocalService;
+
+	@Reference
+	private GuestbookLocalService _guestbookLocalService;
+}
