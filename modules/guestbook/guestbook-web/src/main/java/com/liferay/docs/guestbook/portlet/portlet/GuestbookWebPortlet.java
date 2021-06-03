17eaf2db48e772122a0a30ee950ac12b38906c32
hossein <h.khoshkar@gmail.com>
2021-06-03 09:51:51 +0430

    edit edit_entry.jsp

--- modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookWebPortlet.java
+++ modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookWebPortlet.java
@@ -1,15 +1,23 @@
 package com.liferay.docs.guestbook.portlet.portlet;
 
+import com.liferay.docs.guestbook.model.GuestbookEntry;
 import com.liferay.docs.guestbook.portlet.constants.GuestbookWebPortletKeys;
-
 import com.liferay.docs.guestbook.service.GuestbookEntryLocalService;
 import com.liferay.docs.guestbook.service.GuestbookLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
-
-import javax.portlet.Portlet;
-
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.Portlet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author hossein
@@ -37,4 +45,81 @@ public class GuestbookWebPortlet extends MVCPortlet {
 
 	@Reference
 	private GuestbookLocalService _guestbookLocalService;
+
+	public void addEntry(ActionRequest request, ActionResponse response)
+			throws PortalException {
+
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(
+				GuestbookEntry.class.getName(), request);
+
+		String userName = ParamUtil.getString(request, "name");
+		String email = ParamUtil.getString(request, "email");
+		String message = ParamUtil.getString(request, "message");
+		long guestbookId = ParamUtil.getLong(request, "guestbookId");
+		long entryId = ParamUtil.getLong(request, "entryId");
+
+		if (entryId > 0) {
+
+			try {
+
+//				_guestbookEntryLocalService.updateGuestbookEntry(
+//						serviceContext.getUserId(), guestbookId, entryId, userName,
+//						email, message, serviceContext);
+
+				response.setRenderParameter(
+						"guestbookId", Long.toString(guestbookId));
+
+			}
+			catch (Exception e) {
+				System.out.println(e);
+
+				PortalUtil.copyRequestParameters(request, response);
+
+				response.setRenderParameter(
+						"mvcPath", "/guestbook/edit_entry.jsp");
+			}
+
+		}
+		else {
+
+			try {
+//				_guestbookEntryLocalService.addGuestbookEntry(
+//						serviceContext.getUserId(), guestbookId, userName, email,
+//						message, serviceContext);
+
+				response.setRenderParameter(
+						"guestbookId", Long.toString(guestbookId));
+
+			}
+			catch (Exception e) {
+				System.out.println(e);
+
+				PortalUtil.copyRequestParameters(request, response);
+
+				response.setRenderParameter(
+						"mvcPath", "/guestbook/edit_entry.jsp");
+			}
+		}
+	}
+
+	public void deleteEntry(ActionRequest request, ActionResponse response) throws PortalException {
+		long entryId = ParamUtil.getLong(request, "entryId");
+		long guestbookId = ParamUtil.getLong(request, "guestbookId");
+
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(
+				GuestbookEntry.class.getName(), request);
+
+		try {
+
+			response.setRenderParameter(
+					"guestbookId", Long.toString(guestbookId));
+
+			_guestbookEntryLocalService.deleteGuestbookEntry(entryId);
+		}
+
+		catch (Exception e) {
+			Logger.getLogger(GuestbookWebPortlet.class.getName()).log(
+					Level.SEVERE, null, e);
+		}
+	}
 }
--- modules/guestbook/guestbook-web/src/main/resources/META-INF/resources/init.jsp
+++ modules/guestbook/guestbook-web/src/main/resources/META-INF/resources/init.jsp
@@ -1,6 +1,5 @@
-<%@ include file="/META-INF/resources/init.jsp" %>
-
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
 <%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
 
 <%@ taglib uri="http://liferay.com/tld/aui" prefix="aui" %>
@@ -8,14 +7,6 @@
 <%@ taglib uri="http://liferay.com/tld/theme" prefix="liferay-theme" %>
 <%@ taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui" %>
 
-<portlet:defineObjects />
-
 <liferay-theme:defineObjects />
 
-<portlet:renderURL var="addEntryURL">
-	<portlet:param name="mvcPath" value="/guestbook/edit_entry.jsp"></portlet:param>
-</portlet:renderURL>
-
-<aui:button-row>
-	<aui:button onClick="<%= addEntryURL.toString() %>" value="Add Entry"></aui:button>
-</aui:button-row>
+<portlet:defineObjects />
